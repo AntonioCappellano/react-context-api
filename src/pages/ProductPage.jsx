cb4d0c94116bd742761fd9398af96aa08c06760b
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-20 16:10:36 +0100

    Add budget filtering and improve product display on ProductPage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useBudget } from "../Context/BudgetContext";
 
 export default function ProductPage() {
   const [products, setProducts] = useState([]);
+  const { budgetMode } = useBudget();
 
   function fetchProduct() {
     axios.get("https://fakestoreapi.com/products").then((res) => {
@@ -13,15 +15,19 @@ export default function ProductPage() {
   }
 
   useEffect(fetchProduct, []);
+
+  const filteredProducts = budgetMode
+    ? products.filter((product) => product.price < 30)
+    : products;
   return (
     <div className="container">
       <h1>I Nostri Prodotti</h1>
 
       <div className="row g-3">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <div key={product.id} className="col-3">
             <div className="card h-100">
-              <Link to={"/product/" + product.id}>
+              <Link to={"/product/" + product.id} className="text-decoration-none text-muted">
                 <div className="p-3">
                   <img
                     src={product.image}
@@ -31,6 +37,7 @@ export default function ProductPage() {
                   ></img>
                   <div className="card-body">
                     <h4>{product.title}</h4>
+                    <strong><p>Prezzo: {product.price} euro</p></strong>
                   </div>
                 </div>
               </Link>
